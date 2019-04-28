bfef7f18117e3896e477ecbc1cbda86a7580451c
ihollywhy <yangyd92@gmail.com>
2019-04-27 21:25:27 -0400

    update

--- code/plot.py
+++ code/plot.py
@@ -4,32 +4,38 @@ import numpy as np
 
 cur_dir = os.path.abspath( os.path.dirname(__file__) )
 result_file = os.path.join(cur_dir, "cpu_results.txt")
-shared_file = os.path.join(cur_dir, "shared_results.txt")
+res_list = ['shared_mem']
 
 m_size = []
 cpu_time = []
-gpu_global = []
-gpu_shared = []
-
+gpu_res = []
+print(np.log(2.718))
+tmp = []
 with open(result_file, 'r') as f:
     lines = f.readlines()
     for line in lines:
         res = list(map(int ,line.split(' ')))
         m_size.append(res[0])      
         cpu_time.append(res[1])      
-        gpu_global.append(res[2])
+        tmp.append(res[2])
+gpu_res.append(tmp)
 
-with open(shared_file, 'r') as f:
-    lines = f.readlines()
-    for line in lines:
-        res = list(map(int ,line.split(' ')))
-        gpu_shared.append(res[2])
+for file_name in res_list: 
+    shared_file = os.path.join(cur_dir, file_name+'.txt')
+    tmp = []
+    with open(shared_file, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            res = list(map(int ,line.split(' ')))
+            tmp.append(res[2])
+    gpu_res.append(tmp)
 
-gpu_global = np.array(gpu_global)
+gpu_res = np.array(gpu_res)
 cpu_time = np.array(cpu_time)
 m_size = np.array(m_size)
-plt.plot(np.log(m_size), np.log(cpu_time))
-plt.plot(np.log(m_size), np.log(gpu_global))
-plt.plot(np.log(m_size), np.log(gpu_shared))
-plt.legend(['1 core cpu','global gpu', 'shared gpu'])
+plt.plot(np.log2(m_size), np.log2(cpu_time))
+for gpu_time in gpu_res:
+    plt.plot(np.log2(m_size), np.log2(gpu_time))
+
+plt.legend(['1 core cpu','global_mem']+res_list)
 plt.show()
